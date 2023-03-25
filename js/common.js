95e43c771546ee7493b826bee0f21e370f7cad0a
Ilya <ilya.skobelev.04@mail.ru>
2023-03-25 10:58:52 +0300

    feat: create calculator

---
+++ js/common.js
@@ -0,0 +1,26 @@
+const slider = document.querySelector('.slider');
+const price = document.querySelector('.price');
+const amount = document.querySelector('#amount');
+
+let priceValue = '4.3';
+
+const range = (e) => {
+    const val = e.target.value;
+
+    switch(true) {
+        case val < 1000:
+            priceValue = '4,3';
+            break;
+        case val >= 1000 && val < 3000:
+            priceValue = '4,1';
+            break;
+        case val >= 3000:
+            priceValue = '3,9';
+            break;
+    }
+
+    amount.textContent = `${val} кВт`;
+    price.textContent = priceValue;
+};
+
+slider.addEventListener('input', (e) => range(e));
